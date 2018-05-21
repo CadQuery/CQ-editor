ebe5aade73fdde0ffa36e4d8b837740231526b11
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2018-05-21 11:14:40 +0200

    Refactored object_tree widget to handle object hiding and axes

--- src/widgets/object_tree.py
+++ src/widgets/object_tree.py
@@ -1,5 +1,15 @@
-from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem, QAction
+from PyQt5.QtCore import Qt, pyqtSlot, pyqtSignal
+
+import qtawesome as qta
+
+from OCC.AIS import AIS_ColoredShape, AIS_Line
+from OCC.Quantity import Quantity_NOC_RED as RED
+from OCC.Quantity import Quantity_NOC_GREEN as GREEN
+from OCC.Quantity import Quantity_NOC_BLUE1 as BLUE
+from OCC.Geom import Geom_CylindricalSurface, Geom_Plane, Geom_Circle,\
+     Geom_TrimmedCurve, Geom_Axis1Placement, Geom_Axis2Placement, Geom_Line
+from OCC.gp import gp_Trsf, gp_Vec, gp_Ax3, gp_Dir, gp_Pnt, gp_Ax1
 
 
 
@@ -11,11 +21,14 @@ class TopTreeItem(QTreeWidgetItem):
         
 class ObjectTreeItem(QTreeWidgetItem):
     
-    def __init__(self,*args,**kwargs):
+    def __init__(self,*args,ais=None,shape=None,**kwargs):
         
         super(ObjectTreeItem,self).__init__(*args,**kwargs)
         self.setFlags( self.flags() | Qt.ItemIsUserCheckable)
         self.setCheckState(0,Qt.Checked)
+        
+        self.ais = ais
+        self.shape= shape
 
 class CQRootItem(TopTreeItem):
     
@@ -39,10 +52,16 @@ class HelpersRootItem(TopTreeItem):
 
 class ObjectTree(QTreeWidget):
     
+    sigObjectsAdded = pyqtSignal(list)
+    sigObjectsRemoved = pyqtSignal(list)
+    
     def  __init__(self,parent):
         
         super(ObjectTree,self).__init__(parent)   
         self.setHeaderHidden(True)
+        self.setItemsExpandable(False)
+        self.setRootIsDecorated(False)
+        self.setContextMenuPolicy(Qt.ActionsContextMenu)
         
         self.CQ = CQRootItem()
         self.Imports = ImportRootItem()
@@ -53,5 +72,64 @@ class ObjectTree(QTreeWidget):
         root.addChild(self.Imports)
         root.addChild(self.Helpers)
         
-        self.CQ.addChild(ObjectTreeItem(['Dummy']))
+        self.addAction(QAction(qta.icon('fa.trash'),'Clear all',self,triggered=self.removeObjects))
+        self.addAction(QAction(qta.icon('fa.trash'),'Clear current',self,triggered=self.removeSelected))
+    
+    def addLines(self):
+        
+        origin = (0,0,0)
+        ais_list = []
+        
+        for name,color,direction in zip(('X','Y','Z'),
+                                        (RED,GREEN,BLUE),
+                                        ((1,0,0),(0,1,0),(0,0,1))):
+            line_placement = Geom_Line(gp_Ax1(gp_Pnt(*origin),
+                                       gp_Dir(*direction)))
+            line = AIS_Line(line_placement.GetHandle())
+            line.SetColor(color)
+            
+            self.Helpers.addChild(ObjectTreeItem([name],
+                                                 ais=line))
+            
+            ais_list.append(line)
+            
+        self.sigObjectsAdded.emit(ais_list)
+        self.expandToDepth(1)
+    
+    
+    @pyqtSlot(list)
+    def addObjects(self,objects,root=None):
+        
+        ais_list = []
+        
+        #if root is None:
+        root = self.CQ
+        
+        for name,shape in objects:
+            ais = AIS_ColoredShape(shape)
+            ais_list.append(ais)
+            root.addChild(ObjectTreeItem([name],
+                                         shape=shape,
+                                         ais=ais))
+    
+        self.sigObjectsAdded.emit(ais_list)
+    
+    @pyqtSlot(list)
+    @pyqtSlot(bool)    
+    def removeObjects(self,objects=None):
+        
+        if objects:
+            removed_items_ais = [self.CQ.takeChild(i).ais for i in objects]
+        else:
+            removed_items_ais = [ch.ais for ch in self.CQ.takeChildren()]
+            
+        self.sigObjectsRemoved.emit(removed_items_ais)
+        
+    @pyqtSlot(bool)    
+    def removeSelected(self):
+        
+        ixs = self.selectedIndexes()
+        rows = [ix.row() for ix in ixs]
+        
+        self.removeObjects(rows)
         

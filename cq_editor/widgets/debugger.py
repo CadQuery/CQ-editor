822247800d95451229dc808bad42a4cc6e430ebb
Adam Urbańczyk <adam-urbanczyk@users.noreply.github.com>
2021-03-22 08:03:39 +0100

    Set last traceback

--- cq_editor/widgets/debugger.py
+++ cq_editor/widgets/debugger.py
@@ -246,8 +246,9 @@ class Debugger(QObject,ComponentMixin):
                                    cq_script)
             self.sigLocals.emit(module.__dict__)
         except Exception:
-            self.sigTraceback.emit(sys.exc_info(),
-                                   cq_script)
+            exc_info = sys.exc_info()
+            sys.last_traceback = exc_info[-1]
+            self.sigTraceback.emit(exc_info, cq_script)
     
     @property
     def breakpoints(self):

682660dff49ee2f136cf10c90ff351daae079637
jdegenstein <jdegenstein@users.noreply.github.com>
2022-09-19 10:11:01 -0500

    Enable CQ-editor dark mode for the main window/children

--- cq_editor/main_window.py
+++ cq_editor/main_window.py
@@ -19,6 +19,29 @@ from .mixins import MainMixin
 from .icons import icon
 from .preferences import PreferencesWidget
 
+#DARKMODE edits: https://stackoverflow.com/questions/48256772/dark-theme-for-qt-widgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QPalette, QColor
+app = QApplication([])
+# Force the style to be the same on all OSs:
+app.setStyle("Fusion")
+# Now use a palette to switch to dark colors:
+palette = QPalette()
+palette.setColor(QPalette.Window, QColor(53, 53, 53))
+palette.setColor(QPalette.WindowText, Qt.white)
+palette.setColor(QPalette.Base, QColor(25, 25, 25))
+palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+palette.setColor(QPalette.ToolTipBase, Qt.black)
+palette.setColor(QPalette.ToolTipText, Qt.white)
+palette.setColor(QPalette.Text, Qt.white)
+palette.setColor(QPalette.Button, QColor(53, 53, 53))
+palette.setColor(QPalette.ButtonText, Qt.white)
+palette.setColor(QPalette.BrightText, Qt.red)
+palette.setColor(QPalette.Link, QColor(42, 130, 218))
+palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+palette.setColor(QPalette.HighlightedText, Qt.black)
+app.setPalette(palette)
 
 class MainWindow(QMainWindow,MainMixin):
 

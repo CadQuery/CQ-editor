fbbab21a9c2c88c8da8269797c87ac66a5f94e18
Jeremy Wright <wrightjmf@gmail.com>
2022-11-16 17:13:39 -0500

    Added a very rough try at building an AppImage

---
+++ appimage/appimage-build.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+BUILD_DIR=$(mktemp -d -p "/tmp/" CQeditor-AppImage-build-XXXXXX)
+
+# Clean up the build directory
+cleanup () {
+    if [ -d "$BUILD_DIR" ]; then
+        rm -rf "$BUILD_DIR"
+    fi
+}
+trap cleanup EXIT
+
+# Save the repo root directory
+REPO_ROOT="$(readlink -f $(dirname $(dirname "$0")))"
+OLD_CWD="$(readlink -f .)"
+
+export UPD_INFO="gh-releases-zsync|jmright|CQ-editor|latest|CQeditor*x86_64.AppImage.zsync"
+
+echo $REPO_ROOT
+echo $OLD_CWD
+echo $UPD_INFO
+
+# Set up LinuxDeploy and its conda plugin
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget https://raw.githubusercontent.com/TheAssassin/linuxdeploy-plugin-conda/e714783a1ca6fffeeb9dd15bbfce83831bb196f8/linuxdeploy-plugin-conda.sh
+chmod +x linuxdeploy*.{sh,AppImage}
+
+# LinuxDeploy plugin config
+export CONDA_CHANNELS=conda-forge
+export CONDA_PACKAGES=xorg-libxi
+export PIP_REQUIREMENTS="."
+export PIP_WORKDIR="$REPO_ROOT"
+export PIP_VERBOSE=1
+
+mkdir -p AppDir/usr/share/metainfo/
+cp "$REPO_ROOT"/*.appdata.xml AppDir/usr/share/metainfo/
+
+./linuxdeploy-x86_64.AppImage --appdir AppDir --plugin conda -d "$REPO_ROOT"/CQ-edtior.desktop
+
+export VERSION="0.3-dev"
+
+chmod +x CQ-editor*.AppImage*
+
+# Install the AppImage tool
+wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+chmod +x appimagetool*.AppImage
+./appimagetool*.AppImage AppDir -u "$UPD_INFO"

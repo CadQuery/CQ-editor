fff414d3b67d0e3c5da662f4b418fa3329706ec4
Jeremy Wright <wrightjmf@gmail.com>
2025-02-14 09:18:26 -0500

    Lint check on source code

--- collect_icons.py
+++ collect_icons.py
@@ -2,29 +2,28 @@ from glob import glob
 from subprocess import call
 from os import remove
 
-TEMPLATE = \
-'''<RCC>
+TEMPLATE = """<RCC>
   <qresource prefix="/images">
     {}
   </qresource>
-</RCC>'''
+</RCC>"""
 
-ITEM_TEMPLATE = '<file>{}</file>'
+ITEM_TEMPLATE = "<file>{}</file>"
 
-QRC_OUT = 'icons.qrc'
-RES_OUT = 'src/icons_res.py'
-TOOL = 'pyrcc5'
+QRC_OUT = "icons.qrc"
+RES_OUT = "src/icons_res.py"
+TOOL = "pyrcc5"
 
 items = []
 
-for i in glob('icons/*.svg'):
+for i in glob("icons/*.svg"):
     items.append(ITEM_TEMPLATE.format(i))
-    
-    
-qrc_text = TEMPLATE.format('\n'.join(items))
 
-with open(QRC_OUT,'w') as f:
+
+qrc_text = TEMPLATE.format("\n".join(items))
+
+with open(QRC_OUT, "w") as f:
     f.write(qrc_text)
-    
-call([TOOL,QRC_OUT,'-o',RES_OUT])
+
+call([TOOL, QRC_OUT, "-o", RES_OUT])
 remove(QRC_OUT)
--- run.py
+++ run.py
@@ -3,14 +3,14 @@ import faulthandler
 
 faulthandler.enable()
 
-if 'CASROOT' in os.environ:
-    del os.environ['CASROOT']
+if "CASROOT" in os.environ:
+    del os.environ["CASROOT"]
 
-if sys.platform == 'win32':
+if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 from cq_editor.__main__ import main
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

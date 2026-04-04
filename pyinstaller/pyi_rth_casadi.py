4fd29b87eca854877f16fa97a383bf4ea51c2550
Jeremy Wright <wrightjmf@gmail.com>
2026-04-04 08:18:02 -0400

    Lint fix

--- pyinstaller/pyi_rth_casadi.py
+++ pyinstaller/pyi_rth_casadi.py
@@ -4,7 +4,7 @@ import sys
 # Python 3.8+ uses a restricted DLL search path on Windows. PyInstaller adds
 # _MEIPASS to the search path but not subdirectories. casadi's companion DLLs
 # live in _MEIPASS/casadi/, so register that directory explicitly.
-if sys.platform == 'win32':
-    casadi_dir = os.path.join(sys._MEIPASS, 'casadi')
+if sys.platform == "win32":
+    casadi_dir = os.path.join(sys._MEIPASS, "casadi")
     if os.path.isdir(casadi_dir):
         os.add_dll_directory(casadi_dir)

2210c9d304304d2eb3193a00f67919c383a41c38
Jeremy Wright <wrightjmf@gmail.com>
2026-04-21 07:39:21 -0400

    Work-around for users running on Wayland in Linux

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import sys
+import os
 import faulthandler
+
+# OCP's Xw_Window requires an X11 window ID, so we force XCB so winId() is valid under Wayland/XWayland.
+if sys.platform == "linux" and "QT_QPA_PLATFORM" not in os.environ:
+    os.environ["QT_QPA_PLATFORM"] = "xcb"
 
 # PyInstaller with console=False sets sys.stdout/stderr to None on Windows
 if sys.stdout is None:
